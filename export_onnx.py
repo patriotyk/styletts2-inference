f7745b89742c166891ce2800d09fcc5f81f1e0d4
Serhiy Stetskovych <serhiy.stetskovych@connectwise.com>
2025-03-18 17:01:15 +0200

    Fix export script.

--- export_onnx.py
+++ export_onnx.py
@@ -12,33 +12,34 @@ def export(args):
     else:
         model = StyleTTS2(config_path=args.config, weights_path=args.weights_path, device='cpu')
     
-    styletts_onnx = torch.onnx.export(model,
-                                    {"tokens": model.tokenizer.encode(args.text),
-                                    "voice": model.compute_style(args.audio_prompt),
-                                    "speed": torch.tensor(1.0, dtype=torch.float32),
-                                    "alpha": torch.tensor(0.1, dtype=torch.float32),
-                                    "beta": torch.tensor(0.1, dtype=torch.float32),
-                                    "embedding_scale": torch.tensor(2.0, dtype=torch.float32),
-                                    "diffusion_steps": torch.tensor(4, dtype=torch.int32),
-                                    "s_prev": torch.zeros(1,256, dtype=torch.float32),
-                                    }, f='styletts.onnx', dynamo=False, export_params=True, report=False, verify=True,
-                                    input_names=['tokens',  'speed', 'alpha', 'beta', 'embedding_scale', 'diffusion_steps', 's_prev'],
-                                    output_names=["output_wav", "output_s_pred"],
-                                    training=torch.onnx.TrainingMode.EVAL,
-                                    opset_version=19,
-                                    dynamic_axes={
-                                        'tokens':{0: "seq_length"},
-                                        'output_wav':{0: "seq_length"}
-                                    })
+    torch.onnx.export(model,
+                            {"tokens": model.tokenizer.encode(args.text),
+                            "voice": model.compute_style(args.audio_prompt),
+                            "speed": torch.tensor(1.0, dtype=torch.float32),
+                            "alpha": torch.tensor(0.1, dtype=torch.float32),
+                            "beta": torch.tensor(0.1, dtype=torch.float32),
+                            "embedding_scale": torch.tensor(2.0, dtype=torch.float32),
+                            "diffusion_steps": torch.tensor(4, dtype=torch.int32),
+                            "s_prev": torch.zeros(1,256, dtype=torch.float32),
+                            }, f='styletts.onnx', dynamo=False, export_params=True, report=False, verify=True,
+                            input_names=['tokens',  'speed', 'alpha', 'beta', 'embedding_scale', 'diffusion_steps', 's_prev'],
+                            output_names=["output_wav", "output_s_pred"],
+                            training=torch.onnx.TrainingMode.EVAL,
+                            opset_version=19,
+                            dynamic_axes={
+                                'tokens':{0: "seq_length"},
+                                'output_wav':{0: "seq_length"}
+                            })
     
     
-    for node in styletts_onnx.graph.node:
+    onnx_model = onnx.load('styletts.onnx')
+    for node in onnx_model.graph.node:
         if node.op_type == "Transpose":
             if node.name == "/text_encoder_1/Transpose_7" or node.name == "/text_encoder_1/Transpose_20" or node.name == "/text_encoder_1/Transpose_14":  
                 perm = list(node.attribute[0].ints)  
                 perm = [2 if i == -1 else i for i in perm]  
                 node.attribute[0].ints[:] = perm  
-    onnx.save(model, "styletts.onnx")
+    onnx.save(onnx_model, "styletts.onnx")
     print('Exported!!!')
 
 
